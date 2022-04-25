091d8b8501f478d6b875f90335824a8572533589
RumitPatel <rumit.patel5@gmail.com>
2022-04-25 23:38:14 +0530

    API call to get values into fragment.

--- app/src/main/java/com/example/myapplication/ui/home/quotes/QuotesFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/quotes/QuotesFragment.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication.ui.home.quotes
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.myapplication.R
+import com.example.myapplication.util.Coroutines
+import com.example.myapplication.util.toast
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
@@ -21,5 +27,18 @@ class QuotesFragment : Fragment(), KodeinAware {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.quotes_fragment, container, false)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this, factory).get(QuotesViewModel::class.java)
+
+        Coroutines.main {
+            val quotes = viewModel.quotes.await()
+            quotes.observe(this, Observer {
+                context?.toast(it.size.toString())
+            })
+        }
     }
 }
