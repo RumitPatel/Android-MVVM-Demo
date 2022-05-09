01d4e62e2f3b9b2ec87f284ed1d3d972909a63a8
RumitEmed <rumit.emed@gmail.com>
2022-05-09 17:34:38 +0530

    Code update.

--- app/src/main/java/com/example/myapplication/data/repository/QuotesRepository.kt
+++ app/src/main/java/com/example/myapplication/data/repository/QuotesRepository.kt
@@ -1,7 +1,5 @@
 package com.example.myapplication.data.repository
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.myapplication.data.db.AppDatabase
@@ -12,12 +10,7 @@ import com.example.myapplication.util.Coroutines
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import net.simplifiedcoding.mvvmsampleapp.data.preferences.PreferenceProvider
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
 
-private val MINIMUM_INTERVAL = 6
-
-@RequiresApi(Build.VERSION_CODES.O)
 class QuotesRepository(
     private val api: MyApi,
     private val db: AppDatabase,
@@ -42,7 +35,7 @@ class QuotesRepository(
     private suspend fun fetchQuotes() {
         val lastSavedAt = prefs.getLastSavedAt()
 
-        if (lastSavedAt == null || isFetchNeeded(LocalDateTime.parse(lastSavedAt))) {
+        if (lastSavedAt == null) {
             try {
                 val response = apiRequest { api.getQuotes() }
                 quotes.postValue(response.quotes)
@@ -52,13 +45,9 @@ class QuotesRepository(
         }
     }
 
-    private fun isFetchNeeded(savedAt: LocalDateTime): Boolean {
-        return ChronoUnit.HOURS.between(savedAt, LocalDateTime.now()) > MINIMUM_INTERVAL
-    }
-
     private fun saveQuotes(quotes: List<Quote>) {
         Coroutines.io {
-            prefs.savelastSavedAt(LocalDateTime.now().toString())
+            prefs.saveLastSavedAt(System.currentTimeMillis().toString())
             db.getQuoteDao().saveAllQuotes(quotes)
         }
     }
