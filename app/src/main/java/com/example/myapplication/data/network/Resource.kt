64f1597f215054a79acda9d2534c458229fd531b
RumitPatel <rumit.patel5@gmail.com>
2022-05-21 23:15:36 +0530

    Small change.

---
+++ app/src/main/java/com/example/myapplication/data/network/Resource.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.data.network
+
+import okhttp3.ResponseBody
+
+sealed class Resource<out T> {
+    data class Success<out T>(val value: T) : Resource<T>()
+    data class Failuer(
+        val isNetworkError: Boolean,
+        val errorCode: Int?,
+        val errorBody: ResponseBody?
+    )
+}
